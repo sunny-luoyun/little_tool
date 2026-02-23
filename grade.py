5a040a864e1687f8c3a5b9d3931f0567cc40c2f7
sunny-luoyun <2402580433@qq.com>
2026-02-23 10:35:56 +0800

    0

--- grade.py
+++ grade.py
@@ -4,27 +4,27 @@ import pandas as pd
 # 读取Excel文件
 def read_excel(file_path):
     try:
-        return pd.read_excel(file_path, header=None)  # 没有表头，所以指定header=None
+        return pd.read_excel(file_path, header=None)
     except Exception as e:
         print(f"读取文件 {file_path} 时发生错误: {e}")
         return None
 
 
-# 脱敏身份证号
+
 def mask_id_card(id_card):
     id_card = str(id_card)
     return id_card[:6] + '********' + id_card[-6:]
 
 
-# 脱敏电话号码
+
 def mask_phone(phone):
     phone = str(phone)
     return phone[:3] + '*****' + phone[-3:]
 
 
-# 主程序
+
 def match_names_and_scores(names_file, scores_file, output_file):
-    # 读取名单和成绩
+
     names_df = read_excel(names_file)
     scores_df = read_excel(scores_file)
 
@@ -32,15 +32,15 @@ def match_names_and_scores(names_file, scores_file, output_file):
         print("无法读取文件，请检查文件路径和文件格式。")
         return
 
-    # 为名单表和成绩表添加列名
+
     names_df.columns = ['姓名', '身份证号', '电话']
     scores_df.columns = ['身份证号', '电话', '成绩']
 
-    # 脱敏名单中的身份证号和电话号码
+
     names_df['脱敏身份证号'] = names_df['身份证号'].apply(mask_id_card)
     names_df['脱敏电话'] = names_df['电话'].apply(mask_phone)
 
-    # 匹配身份证号
+
     matched_data = []
     for index, row in names_df.iterrows():
         masked_id_card = row['脱敏身份证号']
@@ -53,10 +53,10 @@ def match_names_and_scores(names_file, scores_file, output_file):
                 '成绩': match['成绩'].values[0]
             })
 
-    # 创建一个新的DataFrame
+
     matched_df = pd.DataFrame(matched_data)
 
-    # 保存到新的Excel文件
+
     try:
         matched_df.to_excel(output_file, index=False)
         print(f"匹配结果已保存到 {output_file}")
---
+++ webspider.py
@@ -0,0 +1,36 @@
+import requests
+
+# 目标地址
+URL = "https://mirror.chromaso.net/thread/1073746784"
+
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+}
+
+
+def fetch_source():
+    print(f"正在请求: {URL} ...")
+    try:
+        response = requests.get(URL, headers=HEADERS, timeout=10)
+        response.encoding = 'utf-8'
+
+        if response.status_code == 200:
+            html_content = response.result if hasattr(response, 'result') else response.text
+
+
+            with open("../m_web/source.txt", "w", encoding="utf-8") as f:
+                f.write(html_content)
+
+            print("✅ 成功！源码已保存至: source.txt")
+
+            print(html_content[:100000])
+        else:
+            print(f"❌ 失败，状态码: {response.status_code}")
+
+    except Exception as e:
+        print(f"💥 发生错误: {e}")
+
+
+if __name__ == "__main__":
+    fetch_source()
